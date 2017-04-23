be2f986bd3c44e9e83c677d4a5e140e69943de5e
ashley <msashleychen@gmail.com>
2017-04-23 13:16:57 -0400

    Added tabs to web

--- model.js
+++ model.js
@@ -189,4 +189,28 @@ setInterval(function() {
 	}
 }, 2000);
 
+function tabChange(evt, pageName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function optionTest(){
+    actors_list  = document.getElementById("actors_list");
+    actor_to_actor.forEach(function(key,value){
+       var option = document.createElement('option');
+       option.value = value;
+       actors_list.appendChild(option);
+    });
+}
 
